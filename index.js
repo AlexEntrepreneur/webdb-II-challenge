ca228c35b1e96a210c4de875563d9dcf13bedace
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-26 14:44:57 +0000

    Implement MVP Endpoint Logic

--- index.js
+++ index.js
@@ -14,26 +14,88 @@ server.use(express.json());
 server.use(helmet());
 
 server.get('/api/zoos', (req, res) => {
-  res.send('Returns list of zoos');
+  db('zoos').select('id', 'name')
+    .then((zoosArray) => {
+      res.json(zoosArray);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Server failed to retrieve Zoos.',
+        serverMessage: `${err}`
+      })
+    });
 });
 
 server.get('/api/zoos/:id', (req, res) => {
-  res.send(`Returns zoo of id ${req.params.id}`);
+  const { id } = req.params;
+  db('zoos').where({ id }).select('id', 'name').first()
+    .then((zooObject) => {
+      res.json(zooObject)
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `Server failed to retrieve Zoo of id ${id}.`,
+        serverMessage: `${err}`
+      })
+    });
 });
 
 server.post('/api/zoos', (req, res) => {
-  res.send(`Added ${JSON.stringify(req.body)} to database`);
+  const { name } = req.body;
+  const newZooObject = { name };
+  db('zoos').insert(newZooObject)
+    .then((newZooId) => {
+      return db('zoos').select('id', 'name');
+    })
+    .then((zoosArray) => {
+      res.json(zoosArray);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Server failed to create a new zoo.',
+        serverMessage: `${err}`
+      })
+    });
 });
 
 server.put('/api/zoos/:id', (req, res) => {
-  res.send(`Updated zoo ${req.params.id} with ${JSON.stringify(req.body)}`);
+  const { id } = req.params;
+  const { name } = req.body;
+  const newZooObject = { name };
+  db('zoos').where({ id }).update(newZooObject)
+    .then((updatedFieldsLength) => {
+      return db('zoos').select('id', 'name');
+    })
+    .then((zoosArray) => {
+      res.json(zoosArray);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `Server failed to update Zoo of id ${id}.`,
+        serverMessage: `${err}`
+      })
+    });
 });
 
 server.delete('/api/zoos/:id', (req, res) => {
-  res.send(`Deleted zoo ${req.params.id}`);
+  const { id } = req.params;
+  db('zoos').where({ id }).del()
+    .then((deletedFieldsLength) => {
+      return db('zoos').select('id', 'name');
+    })
+    .then((zoosArray) => {
+      res.json(zoosArray);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `Server failed to delete Zoo of id ${id}.`,
+        serverMessage: `${err}`
+      })
+    });
 });
 
-const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+const PORT = 3300;
+
+server.listen(PORT, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${PORT} ===\n`);
 });
